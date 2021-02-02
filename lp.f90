27dfe27def30b9563ebdfec498efb1ead3321eb4
Alessandro Pecchia <alessandro.pecchia@ismn.cnr.it>
2021-02-02 12:35:21 +0100

    Remove internal loop

--- lp.f90
+++ lp.f90
@@ -12,22 +12,20 @@ program lp
   real(dp), allocatable :: M(:,:,:)
   real(dp), dimension(neqs+1, neqs+1) :: A
   real(dp) :: t, dt, error
-  integer :: N, ii, iter1, iter2, max_iter1, max_iter2, info, funit
+  integer :: N, ii, iter1, max_iter1, info, funit
   integer, dimension(neqs+1) :: ipv
   real(dp) :: dw_old, tol_dw, max_error
   logical :: is_on_orbit
   character(2) :: citer 
   character(10) :: arg
 
-  if (command_argument_count() < 2) then
-    print*,'lp niter1 iter2'    
+  if (command_argument_count() < 1) then
+    print*,'lp niter1'    
     stop
   end if
 
   call get_command_argument(1, arg)
   read(arg,*) max_iter1
-  call get_command_argument(2, arg)
-  read(arg,*) max_iter2
 
   !
   !  0                                          2pi
@@ -42,7 +40,6 @@ program lp
   eps = 0.1_dp
   tol_dw = 1e-4
   !max_iter1 = 30 
-  !max_iter2 = 1 
   is_on_orbit = .false.
 
 
@@ -95,11 +92,12 @@ program lp
   !    w0 dY/dt = A Y    Y(0) = I 
   ! 1)
   !    w0 dz2/dt = A z2 + d/dt x0   
+  !        z2(0) = 0
   ! 2)  
   !    w0 dy1/dt = A y1 + r(x0(t))    
-  !        y1(0) = y0
+  !        y1(0) = 0
   !
-  ! y1(2*pi) = Y(2*pi) y0 + f1(2*pi)
+  ! y1(2*pi) = f1(2*pi)
   ! y2(2*pi) = dw f2(2*pi)
   !
   ! y(2*pi) = y0 = M*y0 + f1 - dw f2
@@ -107,9 +105,8 @@ program lp
   ! => Condizione 1: (I-M) y0 + f2 dw = f1 
   !    Condizione 2: f(x(0)) y0       = 0
   !
-  ! M = Y(2pi); f2 = z2(2pi); f1 = y1(2pi) - M*y0  
+  ! M = Y(2pi); f2 = z2(2pi); f1 = y1(2pi)
 
-  ! LOOP ESTERNO
   do iter1 = 1, max_iter1
      max_error = 0.0_dp
      do ii = 0, N-1
@@ -168,58 +165,53 @@ program lp
      end do
      z2(:,N) = y20(:)
 
-     ! LOOP Interno per trovare y(0) e dw
-     do iter2 = 1, max_iter2
-
-       ! --------------------------------------------------------
-       ! Risolvere:  w0 dy1/dt = A y1 + r(x0(t))    
-       !                 y1(0) = 0 
-       !
-       ! r(t) = f(x0) - w0 * d/dt x0 
-       !
-       y10 = 0.0_dp
-       do ii = 0, N-1
-          t = ii*dt
-          y(:,ii) = y10(:)
-          call set_points(tt(ii-2:ii+3), x0(:,ii-2:ii+3))
-          call dopri54(sys1, t, dt, y10, y1, error)
-          y10 = y1
-          call clean_points()
-       end do
-       y(:,N) = y10(:)
-
-       if (is_on_orbit) then
-          dw = dot_product(y2,y1)/dot_product(y2,y2)
-       else
-          fx0 = variant(0.0_dp, x0(:,0))
-          A(1:neqs, 1:neqs) = M(:,:,0) - M(:,:,N)
-          A(1:neqs,3) = y2
-          A(3,1:neqs) = fx0
-          A(3,3) = 0.0_dp
-          B(1:neqs)   = y1
-          B(3)   = 0.0_dp
-          
-          call dgesv(3,1,A,3,ipv,B,3,info)
-
-          if (info /= 0) then
-             print*, info
-             stop "Error in dgesv"
-          end if
-
-          y00 = B(1:neqs)
-          dw = B(3)
-          print*,'      iter2=',iter2
-          print*,'      y(0):',y00
-       end if
-        
+     ! --------------------------------------------------------
+     ! Risolvere:  w0 dy1/dt = A y1 + r(x0(t))    
+     !                 y1(0) = 0 
+     !
+     ! r(t) = f(x0) - w0 * d/dt x0 
+     !
+     y10 = 0.0_dp
+     do ii = 0, N-1
+        t = ii*dt
+        y(:,ii) = y10(:)
+        call set_points(tt(ii-2:ii+3), x0(:,ii-2:ii+3))
+        call dopri54(sys1, t, dt, y10, y1, error)
+        y10 = y1
+        call clean_points()
      end do
-     
+     y(:,N) = y10(:)
+
+
+     if (is_on_orbit) then
+        dw = dot_product(y2,y1)/dot_product(y2,y2)
+     else
+        ! Sistema lineare per trovare y(0) e dw
+        fx0 = variant(0.0_dp, x0(:,0))
+        A(1:neqs, 1:neqs) = M(:,:,0) - M(:,:,N)
+        A(1:neqs,3) = y2
+        A(3,1:neqs) = fx0
+        A(3,3) = 0.0_dp
+        B(1:neqs)   = y1
+        B(3)   = 0.0_dp
+        
+        call dgesv(3,1,A,3,ipv,B,3,info)
+
+        if (info /= 0) then
+           print*, info
+           stop "Error in dgesv"
+        end if
+
+        y00 = B(1:neqs)
+        dw = B(3)
+        !print*,'      y(0):',y00
+     end if
+        
      ! copia soluzione y(:,ii) su x0(:)
      write(citer,'(I2.2)') iter1
      open(newunit=funit, file="solution"//citer//".dat")
      do ii = 0, N
         y(:,ii) = matmul(M(:,:,ii),y00) + y(:,ii) - dw*z2(:,ii)
-        !y(:,ii) = y(:,ii) - dw*z2(:,ii)
         x0(:,ii) = x0(:,ii) + y(:,ii)
         write(funit,*) x0(:,ii)
      end do
